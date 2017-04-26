5a0e0a10033bb78e8248b4cecf2f1702618fdd0a
Akash Banerjee <akashbnrj07@gmail.com>
2017-04-26 14:25:46 +0530

    Jasmine test files initial commit

---
+++ spec/ShoppingListMaxItems.spec.js
@@ -0,0 +1,30 @@
+xdescribe("ShoppingListController", function() {
+
+  beforeEach(module('ShoppingListApp'));
+
+  var $controller;
+  var shoppingListController;
+
+  beforeEach(inject(function (_$controller_) {
+    $controller = _$controller_;
+
+    var ShoppingListServiceErrorMock = {};
+    ShoppingListServiceErrorMock.addItem = function (name, quantity) {
+      throw new Error("Test message.");
+    };
+    ShoppingListServiceErrorMock.getItems = function () {
+      return null;
+    };
+
+    shoppingListController =
+      $controller('ShoppingListController',//injection of service(mock)
+                  {ShoppingListService: ShoppingListServiceErrorMock});
+
+  }));
+
+  it("should change error message in controller", function() {
+    shoppingListController.addItem();
+    expect(shoppingListController.errorMessage).toBe("Test message.");
+  });
+
+});
